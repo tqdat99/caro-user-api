ec0972fed8b1486889e45b9b9e005557fe3cd7ea
unknown <tqdat99@gmail.com>
2020-12-09 04:44:12 +0700

    Add routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { getUsers, getUserByUsername, signUp, signIn } = require('../controllers/user');
 var passport = require('passport');
-
 require('../passport/passport')(passport);
-
+const { getUsers, getUserByUsername, signUp, signIn } = require('../controllers/user');
 const userRoutes = express.Router();
 
-userRoutes.get('/', passport.authenticate('jwt', { session: false }), getUsers);
 userRoutes.post('/register', signUp);
 userRoutes.post('/login', signIn);
+userRoutes.get('/', passport.authenticate('jwt', { session: false }), getUsers);
+userRoutes.post('/signup', signUp);
+userRoutes.post('/signin', signIn);
 userRoutes.get('/user', passport.authenticate('jwt', { session: false }), getUserByUsername);
 
 module.exports = userRoutes;
